36a5838c9d0d930d57ff308b4de2ac920b7e7edb
hakanacardev <hakanacar200@gmail.com>
2025-09-10 20:18:15 +0300

    Remove navigation from header
    - Removed navigation menu from header component
    - Removed navigation-related CSS styles
    - Header now only shows centered logo
    - Simplified header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <div className="header_section header_bg">
       <div className="container">
         <Navbar expand="lg" className="navbar-light">
-          <Navbar.Brand as={Link} to="/" style={{ width: '100%', textAlign: 'center' }}>
+          <div style={{ width: '100%', textAlign: 'center' }}>
             <img src="/images/logo.jpeg" width="150" alt="Etçi Kısmet Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/">Ana Sayfa</Nav.Link>
-              <Nav.Link as={Link} to="/menu">Menü</Nav.Link>
-              <Nav.Link as={Link} to="/contact">İletişim</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          </div>
         </Navbar>
       </div>
     </div>
